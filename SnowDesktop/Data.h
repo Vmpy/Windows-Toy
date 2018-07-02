74702d335a1b82af7eba60f25694b9c5c9546569
Pengyi Mu <mupengyi@foxmail.com>
2018-07-02 17:25:01 +0800

    Create Data.h

---
+++ SnowDesktop/Data.h
@@ -0,0 +1,19 @@
+#ifndef DATA_H_INCLUDED
+#define DATA_H_INCLUDED
+
+#include <gdiplus.h>
+class OneSnow
+{
+    public:
+    int x;
+    int y;
+    int Width;
+    int Height;
+    OneSnow(void)
+    {
+    }
+
+};
+
+
+#endif // DATA_H_INCLUDED
