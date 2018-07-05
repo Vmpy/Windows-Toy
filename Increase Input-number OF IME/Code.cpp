b726c22d725d3ed619fb15830d1e29ba22f21306
Pengyi Mu <mupengyi@foxmail.com>
2018-07-05 14:05:47 +0800

    Update Code.cpp

--- Increase Input-number OF IME/Code.cpp
+++ Increase Input-number OF IME/Code.cpp
@@ -27,10 +27,10 @@ int WINAPI WinMain (HINSTANCE hThisInstance,HINSTANCE hPrevInstance,LPSTR lpszAr
     wincl.cbSize = sizeof (WNDCLASSEX);
 
     /* Use default icon and mouse-pointer */
-    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
-    wincl.lpszMenuName = NULL;                 /* No menu */
+    wincl.hIcon = LoadIcon (0, IDI_APPLICATION);
+    wincl.hIconSm = LoadIcon (0, IDI_APPLICATION);
+    wincl.hCursor = LoadCursor (0, IDC_ARROW);
+    wincl.lpszMenuName = 0;                 /* No menu */
     wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
     wincl.cbWndExtra = 0;                      /* structure or the window instance */
     /* Use Windows's default colour as the background of the window */
@@ -51,16 +51,18 @@ int WINAPI WinMain (HINSTANCE hThisInstance,HINSTANCE hPrevInstance,LPSTR lpszAr
            600,                 /* The programs width */
            350,                 /* and height in pixels */
            HWND_DESKTOP,        /* The window is a child-window to desktop */
-           NULL,                /* No menu */
+           0,                /* No menu */
            hThisInstance,       /* Program Instance handler */
-           NULL                 /* No Window Creation data */
+           0                 /* No Window Creation data */
            );
 
     ShowWindow (hwnd, nCmdShow);
+
+    //注册热键
     RegisterHotKey(hwnd,1,MOD_CONTROL,'S');	// Ctrl + S
     RegisterHotKey(hwnd,2,MOD_CONTROL,'E');	// Ctrl + S
 
-    while (GetMessage (&messages, NULL, 0, 0))
+    while (GetMessage (&messages,0,0,0))
     {
         TranslateMessage(&messages);
         DispatchMessage(&messages);
@@ -81,8 +83,8 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
     static HWND TimeEdit;
     static HWND TextTime;
 
+    //数据用于更改系统颜色.
     int aiElements[] ={COLOR_BTNFACE};
-
     DWORD aColors[] ={RGB(255,255,255)};
 
     switch (message)
@@ -91,14 +93,14 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
         {
             HFONT hFont = CreateFont(20,7,0,0,400,FALSE, FALSE, FALSE,DEFAULT_CHARSET,OUT_CHARACTER_PRECIS,CLIP_CHARACTER_PRECIS,DEFAULT_QUALITY,FF_DONTCARE,TEXT("微软雅黑"));
             SetSysColors(1,aiElements,aColors);
-            ChoiceGroupBox = CreateWindowEx(NULL,"BUTTON","按键选择",WS_CHILD|WS_VISIBLE|BS_GROUPBOX,60,45,500,85,hwnd,(HMENU)1,0,0);
-            RadioButtonLeft = CreateWindowEx(NULL,"BUTTON","左键",WS_CHILD|WS_VISIBLE|BS_AUTORADIOBUTTON,5,30,165,40,ChoiceGroupBox,(HMENU)2,0,0);
-            RadioButtonMid = CreateWindowEx(NULL,"BUTTON","中键",WS_CHILD|WS_VISIBLE|BS_AUTORADIOBUTTON,170,30,165,40,ChoiceGroupBox,(HMENU)3,0,0);
-            RadioButtonRight = CreateWindowEx(NULL,"BUTTON","右键",WS_CHILD|WS_VISIBLE|BS_AUTORADIOBUTTON,335,30,160,40,ChoiceGroupBox,(HMENU)4,0,0);
-            TextTime = CreateWindowEx(NULL,"STATIC","点击时间间隔(毫秒):",WS_CHILD|WS_VISIBLE|SS_CENTERIMAGE|SS_CENTER,60,180,135,20,hwnd,(HMENU)5,0,0);
-            TimeEdit = CreateWindowEx(NULL,"EDIT","",WS_CHILD|WS_VISIBLE|WS_BORDER|ES_NUMBER|ES_AUTOHSCROLL,200,180,135,25,hwnd,(HMENU)6,0,0);
-            OkButton = CreateWindowEx(NULL,"BUTTON","开始(S键)",WS_CHILD|WS_VISIBLE|WS_BORDER|BS_PUSHBUTTON|BS_FLAT,410,180,80,30,hwnd,(HMENU)7,0,0);
-            EndButton = CreateWindowEx(NULL,"BUTTON","结束(E键)",WS_CHILD|WS_VISIBLE|WS_BORDER|BS_PUSHBUTTON|BS_FLAT,410,245,80,30,hwnd,(HMENU)8,0,0);
+            ChoiceGroupBox = CreateWindowEx(0,"BUTTON","按键选择",WS_CHILD|WS_VISIBLE|BS_GROUPBOX,60,45,500,85,hwnd,(HMENU)1,0,0);
+            RadioButtonLeft = CreateWindowEx(0,"BUTTON","左键",WS_CHILD|WS_VISIBLE|BS_AUTORADIOBUTTON,5,30,165,40,ChoiceGroupBox,(HMENU)2,0,0);
+            RadioButtonMid = CreateWindowEx(0,"BUTTON","中键",WS_CHILD|WS_VISIBLE|BS_AUTORADIOBUTTON,170,30,165,40,ChoiceGroupBox,(HMENU)3,0,0);
+            RadioButtonRight = CreateWindowEx(0,"BUTTON","右键",WS_CHILD|WS_VISIBLE|BS_AUTORADIOBUTTON,335,30,160,40,ChoiceGroupBox,(HMENU)4,0,0);
+            TextTime = CreateWindowEx(0,"STATIC","点击时间间隔(毫秒):",WS_CHILD|WS_VISIBLE|SS_CENTERIMAGE|SS_CENTER,60,180,135,20,hwnd,(HMENU)5,0,0);
+            TimeEdit = CreateWindowEx(0,"EDIT","",WS_CHILD|WS_VISIBLE|WS_BORDER|ES_NUMBER|ES_AUTOHSCROLL,200,180,135,25,hwnd,(HMENU)6,0,0);
+            OkButton = CreateWindowEx(0,"BUTTON","开始(Ctrl+S)",WS_CHILD|WS_VISIBLE|WS_BORDER|BS_PUSHBUTTON|BS_FLAT,410,180,80,30,hwnd,(HMENU)7,0,0);
+            EndButton = CreateWindowEx(0,"BUTTON","结束(Ctrl+E)",WS_CHILD|WS_VISIBLE|WS_BORDER|BS_PUSHBUTTON|BS_FLAT,410,245,80,30,hwnd,(HMENU)8,0,0);
 
             SendMessage(ChoiceGroupBox,WM_SETFONT,(WPARAM)hFont,0);
             SendMessage(RadioButtonLeft,WM_SETFONT,(WPARAM)hFont,0);
@@ -111,6 +113,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             break;
         }
 
+        //快捷键消息接受.
         case WM_HOTKEY:
         {
             switch(wParam)
@@ -134,10 +137,22 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
 
         case WM_TIMER:
         {
-            if(SendMessage(RadioButtonLeft,BM_GETCHECK,0,0))
+            if(SendMessage(RadioButtonLeft,BM_GETCHECK,0,0) == BST_CHECKED) //获取选择状态
             {
-                mouse_event(MOUSEEVENTF_LEFTDOWN,0,0,0,NULL);
-                mouse_event(MOUSEEVENTF_LEFTUP,0,0,0,NULL);
+                mouse_event(MOUSEEVENTF_LEFTDOWN,0,0,0,0);
+                mouse_event(MOUSEEVENTF_LEFTUP,0,0,0,0);
+                break;
+            }
+            else if(SendMessage(RadioButtonMid,BM_GETCHECK,0,0) == BST_CHECKED)
+            {
+                mouse_event(MOUSEEVENTF_MIDDLEDOWN,0,0,0,0);
+                mouse_event(MOUSEEVENTF_MIDDLEUP,0,0,0,0);
+                break;
+            }
+            else if(SendMessage(RadioButtonRight,BM_GETCHECK,0,0) == BST_CHECKED)
+            {
+                mouse_event(MOUSEEVENTF_RIGHTDOWN,0,0,0,0);
+                mouse_event(MOUSEEVENTF_RIGHTUP,0,0,0,0);
                 break;
             }
             break;
@@ -145,6 +160,9 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
 
         case WM_DESTROY:
         {
+            //解除注册的热键.
+            UnregisterHotKey(hwnd,1);
+            UnregisterHotKey(hwnd,2);
             PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
             break;
         }
