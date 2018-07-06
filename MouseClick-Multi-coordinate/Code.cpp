69c4cf4503e572e576b067f80ef57b4f2da19c40
Pengyi Mu <mupengyi@foxmail.com>
2018-07-06 16:48:01 +0800

    Update Code.cpp

--- MouseClick-Multi-coordinate/Code.cpp
+++ MouseClick-Multi-coordinate/Code.cpp
@@ -5,9 +5,19 @@
 #endif
 
 #include <tchar.h>
+#include <cstring>
 #include <windows.h>
 
 LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+void LoadPosition(POINT*,HWND);
+void ClearPosition(POINT*);
+int FindChar(const char*,char,int);
+
+POINT NowPosition;
+POINT* Checked;
+int SizeOfCheck = 0;
+
+bool Locked = false;
 
 /*  Make the class name into a global variable  */
 TCHAR szClassName[ ] = _T("MouseClick-(Multi-coordinate)");
@@ -41,7 +51,7 @@ int WINAPI WinMain (HINSTANCE hThisInstance,HINSTANCE hPrevInstance,LPSTR lpszAr
 
     /* The class is registered, let's create the program*/
     hwnd = CreateWindowEx (
-           0,                   /* Extended possibilites for variation */
+           WS_EX_TOPMOST,                   /* Extended possibilites for variation */
            szClassName,         /* Classname */
            _T("MouseClick-(Multi-coordinate) By Vmpy"),       /* Title Text */
            WS_OVERLAPPEDWINDOW^WS_THICKFRAME^WS_MAXIMIZEBOX, /* default window */
@@ -57,6 +67,10 @@ int WINAPI WinMain (HINSTANCE hThisInstance,HINSTANCE hPrevInstance,LPSTR lpszAr
 
     /* Make the window visible on the screen */
     ShowWindow (hwnd,nCmdShow);
+
+    RegisterHotKey(hwnd,1,MOD_CONTROL,'S');	// Ctrl + S
+    RegisterHotKey(hwnd,2,MOD_CONTROL,'E');	// Ctrl + E
+    RegisterHotKey(hwnd,3,MOD_CONTROL,'O');	// Ctrl + O
 
     /* Run the message loop. It will run until GetMessage() returns 0 */
     while (GetMessage (&messages, NULL, 0, 0))
@@ -89,10 +103,143 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd,UINT message,WPARAM wParam,LPARAM lP
     static HWND StartButton;
     static HWND EndButton;
 
+    int aiElements[] ={COLOR_BTNFACE};
+    DWORD aColors[] ={RGB(255,255,255)};
+
     switch (message)
     {
+        case WM_TIMER:
+        {
+            switch(wParam)
+            {
+                case 0:
+                {
+                    if(!Locked)
+                    {
+                        char Buffer[10];
+                        GetCursorPos(&NowPosition);
+                        wsprintf(Buffer,"%ld",NowPosition.x);
+                        SendMessage(PositionXEdit,WM_SETTEXT,0,(LPARAM)Buffer);
+                        wsprintf(Buffer,"%ld",NowPosition.y);
+                        SendMessage(PositionYEdit,WM_SETTEXT,0,(LPARAM)Buffer);
+                    }
+                    break;
+                }
+                case 1:
+                {
+                    if(SendMessage(RadioButtonLeft,BM_GETCHECK,0,0) == BST_CHECKED) //获取选择状态
+                    {
+                        for(int i = 0;i < SizeOfCheck;i++)
+                        {
+                            SetCursorPos(Checked[i].x,Checked[i].y);
+                            mouse_event(MOUSEEVENTF_LEFTDOWN,0,0,0,0);
+                            mouse_event(MOUSEEVENTF_LEFTUP,0,0,0,0);
+                        }
+                        break;
+                    }
+                    else if(SendMessage(RadioButtonMid,BM_GETCHECK,0,0) == BST_CHECKED)
+                    {
+                        for(int i = 0;i < SizeOfCheck;i++)
+                        {
+                            SetCursorPos(Checked[i].x,Checked[i].y);
+                            mouse_event(MOUSEEVENTF_MIDDLEDOWN,0,0,0,0);
+                            mouse_event(MOUSEEVENTF_MIDDLEUP,0,0,0,0);
+                        }
+                        break;
+                    }
+                    else if(SendMessage(RadioButtonRight,BM_GETCHECK,0,0) == BST_CHECKED)
+                    {
+                        for(int i = 0;i < SizeOfCheck;i++)
+                        {
+                            SetCursorPos(Checked[i].x,Checked[i].y);
+                            mouse_event(MOUSEEVENTF_RIGHTDOWN,0,0,0,0);
+                            mouse_event(MOUSEEVENTF_RIGHTUP,0,0,0,0);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        KillTimer(hwnd,1);
+                        ClearPosition(Checked);
+                        MessageBox(hwnd,"  请填写未填写完毕的值.","提示",MB_OK|MB_ICONINFORMATION);
+                        break;
+                    }
+                    break;
+                }
+            }
+            break;
+        }
+
+        case WM_HOTKEY:
+        {
+            switch(wParam)
+            {
+                case 1:
+                {
+                    LoadPosition(Checked,PositionList);
+                    if(SizeOfCheck <= 1)
+                    {
+                        MessageBox(hwnd,"  请填入坐标，要求数量大于1","提示",MB_OK|MB_ICONINFORMATION);
+                        break;
+                    }
+                    char Time[10];
+                    SendMessage(TimeEdit,WM_GETTEXT,(WPARAM)10,(LPARAM)Time);
+                    UINT TimeOut = atoi(Time);
+                    SetTimer(hwnd,1,TimeOut,0);
+                    break;
+                }
+                case 2:
+                {
+                    if(SizeOfCheck <= 1)
+                    {
+                        MessageBox(hwnd,"  请填入坐标，要求数量大于1","提示",MB_OK|MB_ICONINFORMATION);
+                        break;
+                    }
+                    KillTimer(hwnd,1);
+                    ClearPosition(Checked); //清理指针空间.
+                    break;
+                }
+                case 3:
+                {
+                    Locked = !Locked;       //锁定和非锁定的状态转化.
+                    break;
+                }
+            }
+            break;
+        }
+
+        case WM_COMMAND:
+        {
+            if((HWND)lParam == AddButton)
+            {
+                char Buffer[20];
+                wsprintf(Buffer,"%ld|%ld",NowPosition.x,NowPosition.y);
+                SendMessage(PositionList,LB_ADDSTRING,0,(LPARAM)Buffer);
+                break;
+            }
+            else if((HWND)lParam == DeleteButton)
+            {
+                int index = SendMessage(PositionList,LB_GETCURSEL,0,0);
+                if(LB_ERR == index)
+                {
+                    break;
+                }
+                SendMessage(PositionList,LB_DELETESTRING,(WPARAM)index,0);
+                break;
+            }
+            else if((HWND)lParam == ClearButton)
+            {
+                SendMessage(PositionList,LB_RESETCONTENT,0,0);
+                SizeOfCheck = 0;
+                break;
+            }
+            break;
+        }
+
         case WM_CREATE:
         {
+            SetSysColors(1,aiElements,aColors);
+            SetTimer(hwnd,0,100,0);
             HFONT hFont = CreateFont(20,7,0,0,400,FALSE, FALSE, FALSE,DEFAULT_CHARSET,OUT_CHARACTER_PRECIS,CLIP_CHARACTER_PRECIS,DEFAULT_QUALITY,FF_DONTCARE,TEXT("微软雅黑"));
 
             TextTime = CreateWindowEx(0,"STATIC","点击时间间隔(ms):",WS_CHILD|WS_VISIBLE|SS_CENTER|SS_CENTERIMAGE,30,40,125,20,hwnd,(HMENU)1,0,0);
@@ -104,7 +251,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd,UINT message,WPARAM wParam,LPARAM lP
             RadioButtonMid = CreateWindowEx(0,"BUTTON","中键",WS_CHILD|WS_VISIBLE|BS_AUTORADIOBUTTON,120,30,80,40,ChoiceGroupBox,(HMENU)5,0,0);
             RadioButtonRight = CreateWindowEx(0,"BUTTON","右键",WS_CHILD|WS_VISIBLE|BS_AUTORADIOBUTTON,225,30,80,40,ChoiceGroupBox,(HMENU)6,0,0);
 
-            PositionList = CreateWindowEx(0,"LISTBOX","坐标",WS_CHILD|WS_BORDER|WS_VISIBLE|LBS_STANDARD^LBS_SORT,25,255,180,210,hwnd,(HMENU)7,0,0);
+            PositionList = CreateWindowEx(0,"LISTBOX","坐标",WS_CHILD|WS_BORDER|WS_VISIBLE|(LBS_STANDARD^LBS_SORT),25,255,180,210,hwnd,(HMENU)7,0,0);
             AddButton = CreateWindowEx(0,"BUTTON","添加",WS_CHILD|WS_BORDER|WS_VISIBLE|BS_PUSHBUTTON|BS_FLAT,25,480,40,25,hwnd,(HMENU)71,0,0);
             DeleteButton = CreateWindowEx(0,"BUTTON","删除",WS_CHILD|WS_BORDER|WS_VISIBLE|BS_PUSHBUTTON|BS_FLAT,75,480,40,25,hwnd,(HMENU)72,0,0);
             ClearButton = CreateWindowEx(0,"BUTTON","清空",WS_CHILD|WS_BORDER|WS_VISIBLE|BS_PUSHBUTTON|BS_FLAT,125,480,40*2,25,hwnd,(HMENU)73,0,0);
@@ -136,12 +283,75 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd,UINT message,WPARAM wParam,LPARAM lP
         }
         case WM_DESTROY:
         {
+            KillTimer(hwnd,1);
             PostQuitMessage (0);
             break;
         }
         default:
             return DefWindowProc (hwnd, message, wParam, lParam);
     }
-
     return 0;
+}
+
+/**
+*为Checked指针（坐标数组）装填信息.
+*@param Pos:已弃用.
+*@param List:ListBox控件句柄.
+*/
+void LoadPosition(POINT* Pos,HWND List)
+{
+    char Buffer[20];
+    SizeOfCheck = SendMessage(List,LB_GETCOUNT,0,0);
+    if(SizeOfCheck <= 1)
+    {
+        return;
+    }
+    Checked = new POINT[SizeOfCheck];
+
+    for(int i = 0; i < SizeOfCheck;i++)
+    {
+        SendMessage(List,LB_GETTEXT,(WPARAM)i,(LPARAM)Buffer);
+        int IndexOfChar = FindChar(Buffer,'|',20);
+        char x[5];
+        char y[5];
+        strncpy(x,Buffer,IndexOfChar);
+        strncpy(y,Buffer+IndexOfChar+1,5);
+        Checked[i].x = atoi(x);
+        Checked[i].y = atoi(y);
+    }
+    return;
+}
+
+/**
+*清理指针空间.
+*@param Pos:已弃用.
+*/
+void ClearPosition(POINT*Pos)
+{
+    if(SizeOfCheck == 1)
+    {
+        delete Checked;
+        Checked = nullptr;
+        return;
+    }
+    delete [] Pos;
+    Checked = nullptr;
+}
+
+/**
+*在字符串strings中找到字符character,返回字符相对于strings中的下标.
+*@param strings:被检索的字符串.
+*@param character:检索字符.
+*@param s:字符串长度.
+*/
+int FindChar(const char* strings,char character,int s)
+{
+    for(int i = 0;i < s;i++)
+    {
+        if(strings[i] == character)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
