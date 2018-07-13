d1edb1bff5b7a96b75890f862892ae85f77a4265
Pengyi Mu <mupengyi@foxmail.com>
2018-07-13 10:58:28 +0800

    Update Functions.h

--- Survival/Functions.h
+++ Survival/Functions.h
@@ -3,8 +3,17 @@
 
 #include "Data.h"
 
+void FirstMenu(Human&);
+void MainMenu(Human&,NatureEvent&);
 float BMI(float,float);
 void InitPlayer(Human&);
+void ReadPlayer(Human&);
+void SavePlayer(Human&);
+
+void DisplayInformation(Human&,NatureEvent&);
+
+void PlayGame(Human&,NatureEvent&);
+
 
 
 #endif // FUNCTIONS_H_INCLUDED
