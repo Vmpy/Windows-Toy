247570b0c2bf889eab4707fd216ff2e2513fca4c
Pengyi Mu <mupengyi@foxmail.com>
2018-07-17 19:16:15 +0800

    Create BulletClass.h

---
+++ PlaneHit/BulletClass.h
@@ -0,0 +1,13 @@
+#ifndef BULLETCLASS_H_INCLUDED
+#define BULLETCLASS_H_INCLUDED
+
+class BulletClass
+{
+    public:
+    int x;
+    int y;
+    int width = 25;
+    int height = 25;
+};
+
+#endif // BULLETCLASS_H_INCLUDED
