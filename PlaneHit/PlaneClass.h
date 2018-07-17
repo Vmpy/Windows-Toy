9a9a8e5e3122a179115d846b6dec5c81e12b0758
Pengyi Mu <mupengyi@foxmail.com>
2018-07-17 20:46:29 +0800

    Update PlaneClass.h

--- PlaneHit/PlaneClass.h
+++ PlaneHit/PlaneClass.h
@@ -3,20 +3,64 @@
 
 #include <windows.h>
 #include "FlyingMonsterClass.h"
+#include "BulletClass.h"
 
 class PlaneClass
 {
     public:
-    int x;
-    int y;
+    int x = 400-100;
+    int y = 450;
     int Width = 100;
     int Height = 100;
-    wchar_t* PngName = (wchar_t*)L"Plane.png";
+    wchar_t* PngName = (wchar_t*)L"Res\\Plane.png";
     int HP = 100;
 
     bool IsDead = false;
 
+    BulletClass Bullet[5];
+
     PlaneClass(void){}
+
+    void Move(int Type)
+    {
+        switch(Type)
+        {
+            case 'W':
+            {
+                if(y > 0)
+                {
+                    y-=5;
+                }
+                break;
+            }
+            case 'S':
+            {
+                if(y < 500)
+                {
+                    y+=5;
+                }
+                break;
+            }
+            case 'A':
+            {
+                if(x > 0)
+                {
+                    x-=5;
+                }
+                break;
+            }
+            case 'D':
+            {
+                if(x < 700)
+                {
+                    x+=5;
+                }
+                break;
+            }
+            default:break;
+        }
+
+    }
 
     void Destroyed(FlyingMonsterClass& Monster)
     {
@@ -37,6 +81,46 @@ class PlaneClass
         if(IntersectRect(&Tmp,&rcSelf,&rcMonster))
         {
             IsDead = true;
+        }
+    }
+
+    void ReFillBullet(void)
+    {
+        for(int i = 0;i < 5;i++)
+        {
+            if(Bullet[i].y < 0)
+            {
+                Bullet[i].x = x+50-10;
+                Bullet[i].y = y-25;
+
+                if(i > 0 && Bullet[i].y == Bullet[i-1].y)       //防止玩家的移动到屏幕顶端再下来的时候图片重叠
+                {
+                    Bullet[i-1].y -= 50;
+                }
+            }
+        }
+    }
+
+    void FillBullet(void)
+    {
+        for(int i = 0;i < 5;i++)
+        {
+            if(!Bullet[i].IsOutPut)
+            {
+                Bullet[i].x = x+50-10;
+                Bullet[i].y = y-25;
+            }
+        }
+    }
+
+    void MoveBullet(void)
+    {
+        for(int i = 0;i < 5;i++)
+        {
+            if(Bullet[i].IsOutPut)
+            {
+                Bullet[i].Move();
+            }
         }
     }
 };
