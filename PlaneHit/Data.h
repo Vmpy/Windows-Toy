232ebcadf6178d1da580ecc4d09d0345defe5558
Pengyi Mu <mupengyi@foxmail.com>
2018-07-18 16:02:04 +0800

    Update Data.h

--- PlaneHit/Data.h
+++ PlaneHit/Data.h
@@ -1,8 +1,8 @@
 #ifndef DATA_H_INCLUDED
 #define DATA_H_INCLUDED
 
-#include "PlaneClass.h"
-#include "FlyingMonsterClass.h"
-#include "BulletClass.h"
+const int MAX_MONSTER  = 3;
+const int MAX_BULLET = 6;
+
 
 #endif // DATA_H_INCLUDED
