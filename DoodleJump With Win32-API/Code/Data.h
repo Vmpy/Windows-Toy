c4c6943c1a2026729d5158d52fd17001f3c4e143
Pengyi Mu <mupengyi@foxmail.com>
2018-08-09 15:10:33 +0800

    Update Data.h

--- DoodleJump With Win32-API/Code/Data.h
+++ DoodleJump With Win32-API/Code/Data.h
@@ -3,37 +3,45 @@
 
 #include <windows.h>
 
-#define MAX_WIDTH 720
-#define MAX_HEIGHT 1280
+#define MAX_WIDTH 360
+#define MAX_HEIGHT 640
 
-class Floor
+#define MAX_ARRSIZE 10
+
+class FloorClass
 {
     public:
-    int Type;
-    int Num;
     int x;
     int y;
 
     int width;
+
+    void ReBorn(void)
+    {
+        x = rand()%MAX_WIDTH;
+        y = 0;
+        width = rand()%50+15;
+    }
 };
 
-class Doodle
+class DoodleClass
 {
     public:
     char FileName[20] = "Res\\Doodle.png";
     int x;
     int y;
     int JumpStartIndex;
-    int Width;
-    int Height;
+    int Width = 10;
+    int Height = 10;
 
-    bool UpOrDown;
-    int MaxJumpHeight;
+    bool UpOrDown = false;
+    int MaxJumpHeight = 25;
     bool IsDead = false;
 
+    //左右移动.
     void Move(char Directiton)
     {
-        switch(Directiton):
+        switch(Directiton)
         {
             case 'D':
             {
@@ -54,13 +62,14 @@ class Doodle
         }
     }
 
-    void GetJumpBase(Floor* Arr,int Size)
+    //获取当前Doodle所踩的踏板下标
+    void GetJumpBase(FloorClass* Arr,int Size)
     {
         int Min;
         int Num[Size];
         for(int i = 0;i< Size;i++)
         {
-            Num[i] = Arr[i]-y;      //当地板在人物上方时为负数
+            Num[i] = Arr[i].y-y;      //当地板在人物上方时为负数
         }
 
         Min = Num[0];
@@ -75,8 +84,20 @@ class Doodle
         }
     }
 
+    //设置跳跃方向（落下[false]或者上升[true])
+    void SetJumpDirection(FloorClass* Arr,int Size)
+    {
+        if((Height + y) == Arr[JumpStartIndex].y)
+        {
+            UpOrDown = true;
+        }
+        if(y == (Arr[JumpStartIndex].y-MaxJumpHeight))
+        {
+            UpOrDown = false;
+        }
+    }
 
-
+    //设置y坐标值
     void Jump(void)
     {
         if(UpOrDown)
@@ -87,6 +108,27 @@ class Doodle
         {
             y++;
         }
+    }
+};
+
+class GameClass
+{
+    public:
+    FloorClass Floor[MAX_ARRSIZE];
+    DoodleClass Doodle;
+
+    GameClass(void)
+    {
+        int BlankHeight = MAX_HEIGHT/MAX_ARRSIZE;
+        for(int i = 0;i < MAX_ARRSIZE;i++)
+        {
+            Floor[i].ReBorn();
+            Floor[i].y = i*BlankHeight;
+        }
+
+        Doodle.JumpStartIndex = 0;
+        Doodle.x = Floor[0].x + 5;
+        Doodle.y = Floor[0].y;
     }
 };
 
